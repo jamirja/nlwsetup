1796a38a801be14d631f748b88ac84d245251237
Jamir <jamiriliev@gmail.com>
2023-03-25 13:38:41 -0100

    fiz mudanças no meu html

--- 09/script.js
+++ 09/script.js
@@ -1,16 +1,20 @@
 var options = {
     chart: {
-      type: 'line'
+      type: 'line',
     },
-    series: [{
+    series: [
+   {
       name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    },
+   ],
     xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 
+        1998, 1999]
+    },
   }
   
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  var chart = new ApexCharts(document.querySelector("#chart"),
+   options)
   
-  chart.render();
+  chart.render()
