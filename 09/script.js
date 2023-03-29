d60f63051a2b608566a2bed8f72870d9bf57fd9f
Jamir <jamiriliev@gmail.com>
2023-03-29 10:13:49 -0100

    changing in the id

--- 09/script.js
+++ 09/script.js
@@ -1,20 +1,104 @@
-var options = {
-    chart: {
-      type: 'line',
+const options = {
+  series: [
+    {
+      name: "cambio",
+      data: [
+        {
+          x: new Date("2018-02-12").getTime(),
+          y:5.18,
+        },
+        {
+          x: new Date("2018-02-13").getTime(),
+          y:5.3,
+        },
+        {
+          x: new Date("2018-02-14").getTime(),
+          y:5.18,
+        },  
+        {
+          x: new Date("2018-02-15").getTime(),
+          y:5.14,
+        },
+        {
+          x: new Date("2018-02-16").getTime(),
+          y:5.18,
+        },
+        {
+          x: new Date("2018-02-17").getTime(),
+          y:5.3,
+        },
+        {
+          x: new Date("2018-02-18").getTime(),
+          y:5.18,
+        },
+        {
+          x: new Date("2018-02-19").getTime(),
+          y:5.2,
+        },
+      ],
     },
-    series: [
-   {
-      name: 'sales',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
     },
-   ],
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 
-        1998, 1999]
+  },
+  datalabels: {
+    enabled: true,
+  },
+  stroke: {
+    curve: "straight",
+  },
+  yaxis: {
+    min: 5,
+    tickAmount: 4,
+    labels: {
+      formatter: (value) => {
+        return value.toFixed(1)
+      },
     },
-  }
-  
-  var chart = new ApexCharts(document.querySelector("#chart"),
+  },
+  xaxis: {
+    labels:{
+      show: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  fill: {
+    gradient: {
+      shaddeIntensity: 1,
+      opacityFron: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100],
+    },
+  },
+  colors: ["#7C3AED"],
+  tooltip: {
+    custom: function ({series, seriesInddex, dataPointIndex,
+    w}) {
+      return `<div class="tooltip">
+      <span>${String(series[seriesInddex][dataPointIndex]).replace('.',',')}</span>
+      <span>${new Date(
+        w.globals.seriesX[seriesInddex][dataPointIndex]
+      ).toLocaleDateString("pt-BR", { 
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+      })}</span>
+      </div>`
+    },
+  },
+}
+
+  const chart = new ApexCharts(document.querySelector("#chart"),
    options)
   
   chart.render()
+  
